fd7d66c0d24fd13ef597ab73953c849530804329
Roderick de Jong <roderick.de.jong@gmail.com>
2012-07-17 10:16:25 +0200

    Added skeleton classes for organizing new rounds.

---
+++ src/Indeler.h
@@ -0,0 +1,42 @@
+#ifndef INDELER_H
+#define INDELER_H
+
+/*************************************************************************
+ * Copyright 2012 Roderick de Jong                                       *
+ *                                                                       *
+ * This file is part of the FreeKeizer project.                          *
+ * FreeKeizer is free software: you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                  *
+ *                                                                       *
+ * FreeKeizer is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with FreeKeizer.  If not, see <http://www.gnu.org/licenses/>.   *
+ *                                                                       *
+ *************************************************************************/
+
+#include "Competitie.h"
+
+
+/**
+ * Produceert mogelijke indelingen voor de volgende ronde van een competitie.
+ * Dit is een abstract class.
+ */
+class Indeler
+{
+	public:
+	Indeler(Competitie* competitie);
+	virtual ~Indeler() { }
+	
+	virtual Ronde* maakIndeling() = 0;
+	
+	protected:
+	Competitie* _competitie;
+};
+
+#endif /* INDELER_H */
---
+++ src/IndelerGeneticAlgorithm.h
@@ -0,0 +1,35 @@
+#ifndef INDELERGENETICALGORITHM_H
+#define INDELERGENETICALGORITHM_H
+
+/*************************************************************************
+ * Copyright 2012 Roderick de Jong                                       *
+ *                                                                       *
+ * This file is part of the FreeKeizer project.                          *
+ * FreeKeizer is free software: you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                  *
+ *                                                                       *
+ * FreeKeizer is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with FreeKeizer.  If not, see <http://www.gnu.org/licenses/>.   *
+ *                                                                       *
+ *************************************************************************/
+
+#include "Indeler.h"
+#include "GeneticAlgorithm.h"
+
+class IndelerGeneticAlgorithm : public Indeler
+{
+	public:
+	IndelerGeneticAlgorithm(Competitie* competitie);
+	virtual ~IndelerGeneticAlgorithm();
+	
+	virtual Ronde* maakIndeling();
+};
+
+#endif /* INDELERGENETICALGORITHM_H */
